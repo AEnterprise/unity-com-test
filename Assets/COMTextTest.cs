85eafdd58771280f69a29028d4aaa7aa65ef7984
AEnterprise <aenterprise2@gmail.com>
2019-10-19 18:05:36 +0200

    basic arduino communication for the sensors

---
+++ Assets/COMTextTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Helpers;
+using UnityEngine;
+
+public class COMTextTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.unityLogger.Log("STARTING");
+        StartCoroutine(
+            COMunicator.COMunicate
+            (
+                "PING",
+                (string s) => Debug.unityLogger.Log(s),
+                () => Debug.unityLogger.LogError("ERROR", "Timed out!")
+            )
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(
+            COMunicator.COMunicate
+            (
+                "SPEEDS",
+                (string s) => GetComponent<TextMesh>().text = s
+            )
+        );
+    }
+}
